182928ccc48b16bee71b7c9a051b3a95d67bd20b
Zoryn Aaron <zoryn4163@gmail.com>
2016-02-29 00:24:00 -0500

    init

---
+++ LessFishingLoss/LessFishingLoss.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using StardewModdingAPI;
+using StardewModdingAPI.Inheritance;
+using StardewModdingAPI.Inheritance.Menus;
+using StardewValley.Menus;
+
+namespace LessFishingLoss
+{
+    public class LessFishingLoss : Mod
+    {
+        public override string Name
+        {
+            get { return "Less Fishing Loss"; }
+        }
+
+        public override string Authour
+        {
+            get { return "Zoryn Aaron"; }
+        }
+
+        public override string Version
+        {
+            get { return "1.0"; }
+        }
+
+        public override string Description
+        {
+            get { return "Causes the amount of loss from not being over a fish to be less."; }
+        }
+
+        private FieldInfo cmg;
+        private bool gotGame;
+        private SBobberBar sb;
+
+        public override void Entry()
+        {
+            Events.UpdateTick += Events_UpdateTick;
+            Events.Initialize += Events_Initialize;
+        }
+
+        void Events_Initialize()
+        {
+            cmg = SGame.StaticFields.First(x => x.Name == "activeClickableMenu");
+        }
+
+        void Events_UpdateTick()
+        {
+            if (cmg != null && cmg.GetValue(null) != null)
+            {
+                if (cmg.GetValue(null).GetType() == typeof(BobberBar))
+                {
+                    if (!gotGame)
+                    {
+                        gotGame = true;
+                        BobberBar b = (BobberBar)cmg.GetValue(null);
+                        sb = SBobberBar.ConstructFromBaseClass(b);
+                    }
+                    else
+                    {
+                        if (!sb.bobberInBar)
+                        {
+                            //player is failing
+                            sb.distanceFromCatching += 2 / 1000f;
+                        }
+                    }
+                }
+                else
+                {
+                    gotGame = false;
+                    sb = null;
+                }
+            }
+        }
+    }
+}
---
+++ StardewTestMod/Class1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+using StardewModdingAPI;
+using StardewModdingAPI.Inheritance;
+using StardewModdingAPI.Inheritance.Menus;
+using StardewValley.Menus;
+
+namespace StardewTestMod
+{
+    public class TestMod : Mod
+    {
+        public override string Name
+        {
+            get { return "Test Mod"; }
+        }
+
+        public override string Authour
+        {
+            get { return "Zoryn Aaron"; }
+        }
+
+        public override string Version
+        {
+            get { return "0.0.1Test"; }
+        }
+
+        public override string Description
+        {
+            get { return "A Test Mod"; }
+        }
+
+        public override void Entry()
+        {
+            Console.WriteLine("Test Mod Has Loaded");
+            Program.LogError("Test Mod can call to Program.cs in the API");
+            Program.LogColour(ConsoleColor.Magenta, "Test Mod is just a tiny DLL file in AppData/Roaming/StardewValley/Mods");
+            
+            //Subscribe to an event from the modding API
+            Events.KeyPressed += Events_KeyPressed;
+            Events.UpdateTick += Events_UpdateTick;
+            Events.Initialize += Events_Initialize;
+        }
+
+        private FieldInfo cmg;
+        private bool gotGame;
+        private SBobberBar sb;
+
+        void Events_Initialize()
+        {
+            cmg = SGame.StaticFields.First(x => x.Name == "activeClickableMenu");
+        }
+
+        void Events_UpdateTick()
+        {
+            if (cmg != null && cmg.GetValue(null) != null)
+            {
+                if (cmg.GetValue(null).GetType() == typeof(BobberBar))
+                {
+                    if (!gotGame)
+                    {
+                        gotGame = true;
+                        BobberBar b = (BobberBar)cmg.GetValue(null);
+                        sb = SBobberBar.ConstructFromBaseClass(b);
+                    }
+                    else
+                    {
+                        sb.bobberPosition = Extensions.Random.Next(0, 750);
+                        sb.treasure = true;
+                        sb.distanceFromCatching = 0.5f;
+                    }
+                }
+                else
+                {
+                    gotGame = false;
+                    sb = null;
+                }
+            }
+        }
+
+        void Events_KeyPressed(Keys key)
+        {
+            Console.WriteLine("TestMod sees that the following key was pressed: " + key);
+        }
+    }
+}
